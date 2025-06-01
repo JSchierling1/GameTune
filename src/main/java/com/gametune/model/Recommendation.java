6b30b544276e006dce08c854f22ca54de2418c56
Jeremy Schierling <jeremy-andreas.schierling@stud.uni-bamberg.de>
2025-06-02 01:34:49 +0200

    Implement Requirements Check

--- src/main/java/com/gametune/model/Recommendation.java
+++ src/main/java/com/gametune/model/Recommendation.java
@@ -20,4 +20,29 @@ public class Recommendation {
     private String resolution;
     private String frameGen;
     private int estimatedFps;
+
+    public boolean isPlayable(HardwareProfile hardwareProfile, Game game){
+        //Whether the game is playable on this hardware profile with the given settings
+        boolean playable;
+        if (hardwareProfile.getGpu().getScore()>= game.getMinRequirements().getGpu().getScore() && hardwareProfile.getCpu().getScore()>= game.getMinRequirements().getCpu().getScore() && hardwareProfile.getRam() >= game.getMinRequirements().getRam()) {
+            playable = true;
+        } else {
+            playable = false;
+        }
+        return playable;
+    }
+
+    public boolean recRequirementsMet(HardwareProfile hardwareProfile, Game game) {
+        //Whether the game is playable on this hardware profile with the given settings
+        boolean recRequirementsMet;
+        if (hardwareProfile.getGpu().getScore()>= game.getRecRequirements().getGpu().getScore() && hardwareProfile.getCpu().getScore()>= game.getRecRequirements().getCpu().getScore() && hardwareProfile.getRam() >= game.getRecRequirements().getRam()) {
+            recRequirementsMet = true;
+        } else {
+            recRequirementsMet = false;
+        }
+        return recRequirementsMet;
+    }
+
+
+
 }
